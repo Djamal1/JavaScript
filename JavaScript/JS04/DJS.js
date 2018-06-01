977387a4e1feec08099365352d9c4b55e252e05c
Djamal <32506792+Djamal1@users.noreply.github.com>
2018-06-01 22:18:37 +0300

    Урок по JS!
    С новым годом! Хотя сейчас лето, а насрать!

---
+++ JavaScript/JS04/DJS.js
@@ -0,0 +1,18 @@
+"use strict"
+
+alert(1 / 0); // Infinity - бесконечное число
+alert("none" * 5); // NaN - Ошибка вычеслений
+
+var check = true;
+check = false;
+
+var age = null;
+
+var x;
+alert(x);
+
+var x = 123;
+x = "undefined";
+alert(x);
+
+var user = {name: "Вася"};
---
+++ JavaScript/JS04/OTJS.js
@@ -0,0 +1,15 @@
+"use strict"
+
+typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof {} // "object"
+
+typeof null // "object"  (1)
+
+typeof function(){} // "function"  (2)
