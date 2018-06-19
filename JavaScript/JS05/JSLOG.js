c2ba3bf059a34640c8d87d19797ee30fc187636f
Djamal <32506792+Djamal1@users.noreply.github.com>
2018-06-20 01:07:39 +0300

    New lesson 5!

---
+++ JavaScript/JS05/JSLOG.js
@@ -0,0 +1,19 @@
+"use strict"
+
+alert(2 > 1);
+alert(3 < 2);
+alert(2 == 2);
+alert(2 != 1);
+alert(2 != 2);
+
+var a = true;
+var b = 5 > 4;
+
+alert(a == b);
+
+alert("А" > "Б");
+alert("а" > "Я");
+alert("Привет" < "Прив");
+alert("Ваня" > "Вася");
+alert("Привет" > "Hello");
+alert("1" < "2");
